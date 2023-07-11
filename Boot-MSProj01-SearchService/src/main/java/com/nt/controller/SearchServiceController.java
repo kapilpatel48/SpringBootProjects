65824acf24df05d141e7be627a71f667a7bf8d85
kapil patel <Lenovo@DESKTOP-O89MCCH>
2023-07-11 10:35:03 +0530

    eureka

---
+++ Boot-MSProj01-SearchService/src/main/java/com/nt/controller/SearchServiceController.java
@@ -0,0 +1,15 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchServiceController {
+
+	@GetMapping("/display")
+	public String displaayMessage() {
+		return "welcome to flipkart searchService";
+	}
+}
